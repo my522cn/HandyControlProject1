5fe41b06510f07594455cd48dab9188fa56871ce
Neo Xiao <my522cn@hotmail.com>
2021-12-17 08:37:39 +0800

    添加项目文件。

---
+++ HandyControlProject1/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using HandyControlProject1.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace HandyControlProject1
+{
+    public class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
