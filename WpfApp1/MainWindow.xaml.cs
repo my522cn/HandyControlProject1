0fd761e11f8df90e519a6e09b3e4d09c9b9ecc88
Neo Xiao <my522cn@hotmail.com>
2021-12-20 09:41:21 +0800

    增加官方HandyControl WPF Demo

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HandyControl.Controls.MessageBox.Show("xxxxx");
+        }
+    }
+}
